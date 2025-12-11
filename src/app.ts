a69918090eea5fcecb5c68a1e43eeee6700514cd
paollaalvesgeraldo1 <paollaalvesgeraldo1@gmail.com>
2025-12-11 20:18:43 -0300

    Atualiza dependências do projeto e corrige importação do repositório

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
-import { createTable } from "./repositories/initRepository";
+import { createTable } from "./repositories/InitRepository";
 import usuarioRoutes from "./routes/usuarioRoutes";
+import feedbackRoutes from "./routes/feedbackRoutes";
+import materialRoutes from "./routes/materialRoutes";
 
 const app = express();
 app.use(express.json());
 createTable();
 
 app.use("/usuarios", usuarioRoutes);
+app.use("/feedback", feedbackRoutes);
+app.use("/material", materialRoutes);
+
 
 export default app;
